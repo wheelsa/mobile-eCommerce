d8eee4462ecf80e5ab422d0e808e92d154c107f4
Seth Wheeler <wheeler.seth@gmail.com>
2020-05-07 13:27:19 -0600

    setup redux for state mgmt

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FlatList, Text } from 'react-native'
+import { useSelector } from 'react-redux'
+
+const ProductOverviewScreen = (props) => {
+  const products = useSelector( state => state.products.availableProducts)
+
+  return(
+    <FlatList data={products} renderItem={itemData => <Text>{itemData.item.title}</Text>}/>
+  )
+}
+
+export default ProductOverviewScreen
